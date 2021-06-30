f8ba3e99ed0d6a04bab9cebbd78c7a76917c26f5
Marvin März <marvin.maerz@gmail.com>
2021-06-30 13:43:37 +0200

    test/added tests for json serlization and over all library

---
+++ test/steps/step_test.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:survey_kit/src/steps/predefined_steps/instruction_step.dart';
+import 'package:survey_kit/src/steps/step.dart';
+import 'package:survey_kit/survey_kit.dart';
+
+void main() {
+  test('instruction step is the same created by json and code', () {
+    final jsonStr = '''
+    {
+      "stepIdentifier": {
+        "id": "1"
+      },
+      "type": "intro",
+      "title": "Welcome to the\\nQuickBird Studios\\nHealth Survey",
+      "text": "Get ready for a bunch of super random questions!",
+      "buttonText": "Let's go!"
+    }
+    ''';
+    final jsonStep = Step.fromJson(json.decode(jsonStr));
+    final step = InstructionStep(
+      stepIdentifier: StepIdentifier(id: "1"),
+      title: 'Welcome to the\nQuickBird Studios\nHealth Survey',
+      text: 'Get ready for a bunch of super random questions!',
+      buttonText: 'Let\'s go!',
+    );
+
+    expect(step, jsonStep);
+  });
+
+  test('throw step not defined exception of type is unvalid', () {
+    final jsonStr = '''
+    {
+      "stepIdentifier": {
+        "id": "1"
+      },
+      "type": "undefined",
+      "title": "Welcome to the\\nQuickBird Studios\\nHealth Survey",
+      "text": "Get ready for a bunch of super random questions!",
+      "buttonText": "Let's go!"
+    }
+    ''';
+    expect(() => Step.fromJson(json.decode(jsonStr)), throwsException);
+  });
+}
