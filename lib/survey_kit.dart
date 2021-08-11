a66ead5d7eb673e53146be2a0557df42c8aba71f
Marvin März <marvin.maerz@gmail.com>
2021-08-11 13:07:18 +0200

    feature/added way to override the survey app bar

--- lib/survey_kit.dart
+++ lib/survey_kit.dart
@@ -79,3 +79,4 @@ export 'src/navigator/rules/rule_not_defined_exception.dart';
 export 'src/steps/predefined_steps/answer_format_not_defined_exception.dart';
 
 export 'src/widget/survey_progress_configuration.dart';
+export 'src/views/widget/survey_app_bar.dart';
