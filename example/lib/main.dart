25ec142ddba79eb9e3c647ca58d634da6c4c9d1d
Gabriel Soares <soares1902@outlook.com>
2022-09-16 14:15:05 -0300

    Return the main file to the standard
    Return the main file to the standard

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,7 +24,7 @@ class _MyAppState extends State<MyApp> {
           child: Align(
             alignment: Alignment.center,
             child: FutureBuilder<Task>(
-              future: getSampleTask(),
+              future: getJsonTask(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.done &&
                     snapshot.hasData &&
@@ -33,7 +33,6 @@ class _MyAppState extends State<MyApp> {
                   return SurveyKit(
                     onResult: (SurveyResult result) {
                       print(result.finishReason);
-                      print(result.results[1].results);
                       Navigator.pushNamed(context, '/');
                     },
                     task: task,
@@ -42,7 +41,109 @@ class _MyAppState extends State<MyApp> {
                       'cancel': 'Cancel',
                       'next': 'Next',
                     },
-                    themeData: _surveyThemeData(context),
+                    themeData: Theme.of(context).copyWith(
+                      colorScheme: ColorScheme.fromSwatch(
+                        primarySwatch: Colors.cyan,
+                      ).copyWith(
+                        onPrimary: Colors.white,
+                      ),
+                      primaryColor: Colors.cyan,
+                      backgroundColor: Colors.white,
+                      appBarTheme: const AppBarTheme(
+                        color: Colors.white,
+                        iconTheme: IconThemeData(
+                          color: Colors.cyan,
+                        ),
+                        titleTextStyle: TextStyle(
+                          color: Colors.cyan,
+                        ),
+                      ),
+                      iconTheme: const IconThemeData(
+                        color: Colors.cyan,
+                      ),
+                      textSelectionTheme: TextSelectionThemeData(
+                        cursorColor: Colors.cyan,
+                        selectionColor: Colors.cyan,
+                        selectionHandleColor: Colors.cyan,
+                      ),
+                      cupertinoOverrideTheme: CupertinoThemeData(
+                        primaryColor: Colors.cyan,
+                      ),
+                      outlinedButtonTheme: OutlinedButtonThemeData(
+                        style: ButtonStyle(
+                          minimumSize: MaterialStateProperty.all(
+                            Size(150.0, 60.0),
+                          ),
+                          side: MaterialStateProperty.resolveWith(
+                            (Set<MaterialState> state) {
+                              if (state.contains(MaterialState.disabled)) {
+                                return BorderSide(
+                                  color: Colors.grey,
+                                );
+                              }
+                              return BorderSide(
+                                color: Colors.cyan,
+                              );
+                            },
+                          ),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                          textStyle: MaterialStateProperty.resolveWith(
+                            (Set<MaterialState> state) {
+                              if (state.contains(MaterialState.disabled)) {
+                                return Theme.of(context)
+                                    .textTheme
+                                    .button
+                                    ?.copyWith(
+                                      color: Colors.grey,
+                                    );
+                              }
+                              return Theme.of(context)
+                                  .textTheme
+                                  .button
+                                  ?.copyWith(
+                                    color: Colors.cyan,
+                                  );
+                            },
+                          ),
+                        ),
+                      ),
+                      textButtonTheme: TextButtonThemeData(
+                        style: ButtonStyle(
+                          textStyle: MaterialStateProperty.all(
+                            Theme.of(context).textTheme.button?.copyWith(
+                                  color: Colors.cyan,
+                                ),
+                          ),
+                        ),
+                      ),
+                      textTheme: TextTheme(
+                        headline2: TextStyle(
+                          fontSize: 28.0,
+                          color: Colors.black,
+                        ),
+                        headline5: TextStyle(
+                          fontSize: 24.0,
+                          color: Colors.black,
+                        ),
+                        bodyText2: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.black,
+                        ),
+                        subtitle1: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      inputDecorationTheme: InputDecorationTheme(
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
                     surveyProgressbarConfiguration: SurveyProgressConfiguration(
                       backgroundColor: Colors.white,
                     ),
@@ -57,122 +158,93 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  ThemeData _surveyThemeData(BuildContext context) {
-    return Theme.of(context).copyWith(
-      colorScheme: ColorScheme.fromSwatch(
-        primarySwatch: Colors.cyan,
-      ).copyWith(
-        onPrimary: Colors.white,
-      ),
-      primaryColor: Colors.cyan,
-      backgroundColor: Colors.white,
-      appBarTheme: const AppBarTheme(
-        color: Colors.white,
-        iconTheme: IconThemeData(
-          color: Colors.cyan,
-        ),
-        titleTextStyle: TextStyle(
-          color: Colors.cyan,
-        ),
-      ),
-      iconTheme: const IconThemeData(
-        color: Colors.cyan,
-      ),
-      textSelectionTheme: TextSelectionThemeData(
-        cursorColor: Colors.cyan,
-        selectionColor: Colors.cyan,
-        selectionHandleColor: Colors.cyan,
-      ),
-      cupertinoOverrideTheme: CupertinoThemeData(
-        primaryColor: Colors.cyan,
-      ),
-      outlinedButtonTheme: OutlinedButtonThemeData(
-        style: ButtonStyle(
-          minimumSize: MaterialStateProperty.all(
-            Size(150.0, 60.0),
-          ),
-          side: MaterialStateProperty.resolveWith(
-            (Set<MaterialState> state) {
-              if (state.contains(MaterialState.disabled)) {
-                return BorderSide(
-                  color: Colors.grey,
-                );
-              }
-              return BorderSide(
-                color: Colors.cyan,
-              );
-            },
-          ),
-          shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-          ),
-          textStyle: MaterialStateProperty.resolveWith(
-            (Set<MaterialState> state) {
-              if (state.contains(MaterialState.disabled)) {
-                return Theme.of(context).textTheme.button?.copyWith(
-                      color: Colors.grey,
-                    );
-              }
-              return Theme.of(context).textTheme.button?.copyWith(
-                    color: Colors.cyan,
-                  );
-            },
-          ),
-        ),
-      ),
-      textButtonTheme: TextButtonThemeData(
-        style: ButtonStyle(
-          textStyle: MaterialStateProperty.all(
-            Theme.of(context).textTheme.button?.copyWith(
-                  color: Colors.cyan,
-                ),
-          ),
-        ),
-      ),
-      textTheme: TextTheme(
-        headline2: TextStyle(
-          fontSize: 28.0,
-          color: Colors.black,
-        ),
-        headline5: TextStyle(
-          fontSize: 24.0,
-          color: Colors.black,
-        ),
-        bodyText2: TextStyle(
-          fontSize: 18.0,
-          color: Colors.black,
-        ),
-        subtitle1: TextStyle(
-          fontSize: 18.0,
-          color: Colors.black,
-        ),
-      ),
-      inputDecorationTheme: InputDecorationTheme(
-        labelStyle: TextStyle(
-          color: Colors.black,
-        ),
-      ),
-    );
-  }
-
   Future<Task> getSampleTask() {
     var task = NavigableTask(
       id: TaskIdentifier(),
       steps: [
-        QuestionStep(
-          title: 'Upload a file',
-          answerFormat: ImageAnswerFormat(
-            defaultValue: '',
-            hint: 'file',
-          ),
-          isOptional: true,
-        ),
         InstructionStep(
           title: 'Welcome to the\nQuickBird Studios\nHealth Survey',
           text: 'Get ready for a bunch of super random questions!',
           buttonText: 'Let\'s go!',
+        ),
+        QuestionStep(
+          title: 'How old are you?',
+          answerFormat: IntegerAnswerFormat(
+            defaultValue: 25,
+            hint: 'Please enter your age',
+          ),
+          isOptional: true,
+        ),
+        QuestionStep(
+          title: 'Medication?',
+          text: 'Are you using any medication',
+          answerFormat: BooleanAnswerFormat(
+            positiveAnswer: 'Yes',
+            negativeAnswer: 'No',
+            result: BooleanResult.POSITIVE,
+          ),
+        ),
+        QuestionStep(
+          title: 'Tell us about you',
+          text:
+              'Tell us about yourself and why you want to improve your health.',
+          answerFormat: TextAnswerFormat(
+            maxLines: 5,
+            validationRegEx: "^(?!\s*\$).+",
+          ),
+        ),
+        QuestionStep(
+          title: 'Select your body type',
+          answerFormat: ScaleAnswerFormat(
+            step: 1,
+            minimumValue: 1,
+            maximumValue: 5,
+            defaultValue: 3,
+            minimumValueDescription: '1',
+            maximumValueDescription: '5',
+          ),
+        ),
+        QuestionStep(
+          title: 'Known allergies',
+          text: 'Do you have any allergies that we should be aware of?',
+          isOptional: false,
+          answerFormat: MultipleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'Penicillin', value: 'Penicillin'),
+              TextChoice(text: 'Latex', value: 'Latex'),
+              TextChoice(text: 'Pet', value: 'Pet'),
+              TextChoice(text: 'Pollen', value: 'Pollen'),
+            ],
+          ),
+        ),
+        QuestionStep(
+          title: 'Done?',
+          text: 'We are done, do you mind to tell us more about yourself?',
+          isOptional: true,
+          answerFormat: SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'Yes', value: 'Yes'),
+              TextChoice(text: 'No', value: 'No'),
+            ],
+            defaultSelection: TextChoice(text: 'No', value: 'No'),
+          ),
+        ),
+        QuestionStep(
+          title: 'When did you wake up?',
+          answerFormat: TimeAnswerFormat(
+            defaultValue: TimeOfDay(
+              hour: 12,
+              minute: 0,
+            ),
+          ),
+        ),
+        QuestionStep(
+          title: 'When was your last holiday?',
+          answerFormat: DateAnswerFormat(
+            minDate: DateTime.utc(1970),
+            defaultDate: DateTime.now(),
+            maxDate: DateTime.now(),
+          ),
         ),
         CompletionStep(
           stepIdentifier: StepIdentifier(id: '321'),
@@ -183,14 +255,14 @@ class _MyAppState extends State<MyApp> {
       ],
     );
     task.addNavigationRule(
-      forTriggerStepIdentifier: task.steps[2].stepIdentifier,
+      forTriggerStepIdentifier: task.steps[6].stepIdentifier,
       navigationRule: ConditionalNavigationRule(
         resultToStepIdentifierMapper: (input) {
           switch (input) {
             case "Yes":
               return task.steps[0].stepIdentifier;
             case "No":
-              return task.steps[3].stepIdentifier;
+              return task.steps[7].stepIdentifier;
             default:
               return null;
           }
