1a31247a05e667d957b55f532279aa64b9b80f44
Gabriel Soares <40280489+soaresg@users.noreply.github.com>
2022-09-29 04:30:56 -0300

    File step (#87)
    * file step structure created
    
    * initial logic of take a picture
    
    * add logic of sending image
    
    * Adjustments in view
    
    Creation of a text showing the name of the file for a feedback to the user.
    
    * Return the main file to the standard
    
    Return the main file to the standard
    
    * Restored files to standard
    
    Restored the example files, AndroidManifest.xml, build.gradle and gradle-wrapper.properties to standard.
    
    * Issue Correction
    
    Issue corretion for flutter analyze
    
    * Requested Changes
    
    Co-authored-by: Gabriel Soares <gabriel.soares@simrede.com.br>

--- lib/survey_kit.dart
+++ lib/survey_kit.dart
@@ -1,91 +1,77 @@
 library survery_kit;
 
-export 'src/survey_kit.dart';
-
 export 'src/answer_format/answer_format.dart';
 export 'src/answer_format/boolean_answer_format.dart';
+export 'src/answer_format/date_answer_format.dart';
 export 'src/answer_format/double_answer_format.dart';
+export 'src/answer_format/image_answer_format.dart';
 export 'src/answer_format/integer_answer_format.dart';
+export 'src/answer_format/multi_double.dart';
 export 'src/answer_format/multiple_choice_answer_format.dart';
 export 'src/answer_format/multiple_double_answer_format.dart';
 export 'src/answer_format/scale_answer_format.dart';
 export 'src/answer_format/single_choice_answer_format.dart';
 export 'src/answer_format/text_answer_format.dart';
 export 'src/answer_format/text_choice.dart';
-export 'src/answer_format/multi_double.dart';
-export 'src/answer_format/date_answer_format.dart';
 export 'src/answer_format/time_answer_formart.dart';
-
-export 'src/navigator/task_navigator.dart';
-export 'src/navigator/ordered_task_navigator.dart';
+export 'src/configuration/app_bar_configuration.dart';
+export 'src/controller/survey_controller.dart';
 export 'src/navigator/navigable_task_navigator.dart';
-export 'src/navigator/rules/navigation_rule.dart';
-export 'src/navigator/rules/direct_navigation_rule.dart';
+export 'src/navigator/ordered_task_navigator.dart';
 export 'src/navigator/rules/conditional_navigation_rule.dart';
-
-export 'src/presenter/survey_presenter.dart';
+export 'src/navigator/rules/direct_navigation_rule.dart';
+export 'src/navigator/rules/navigation_rule.dart';
+export 'src/navigator/rules/rule_not_defined_exception.dart';
+export 'src/navigator/task_navigator.dart';
 export 'src/presenter/survey_event.dart';
+export 'src/presenter/survey_presenter.dart';
 export 'src/presenter/survey_state.dart';
-
 export 'src/result/question/boolean_question_result.dart';
+export 'src/result/question/date_question_result.dart';
 export 'src/result/question/double_question_result.dart';
+export 'src/result/question/image_question_result.dart';
 export 'src/result/question/integer_question_result.dart';
 export 'src/result/question/multiple_choice_question_result.dart';
 export 'src/result/question/multiple_double_question_result.dart';
 export 'src/result/question/scale_question_result.dart';
 export 'src/result/question/single_choice_question_result.dart';
 export 'src/result/question/text_question_result.dart';
-export 'src/result/question/date_question_result.dart';
 export 'src/result/question/time_question_result.dart';
-
-export 'src/result/step/completion_step_result.dart';
-export 'src/result/step/instruction_step_result.dart';
-
-export 'src/result/survey/survey_result.dart';
-
 export 'src/result/question_result.dart';
 export 'src/result/result.dart';
+export 'src/result/step/completion_step_result.dart';
+export 'src/result/step/instruction_step_result.dart';
 export 'src/result/step_result.dart';
-
-export 'src/steps/step.dart';
-
+export 'src/result/survey/survey_result.dart';
 export 'src/steps/identifier/identifier.dart';
 export 'src/steps/identifier/step_identifier.dart';
-
+export 'src/steps/predefined_steps/answer_format_not_defined_exception.dart';
 export 'src/steps/predefined_steps/completion_step.dart';
 export 'src/steps/predefined_steps/instruction_step.dart';
 export 'src/steps/predefined_steps/question_step.dart';
-
+export 'src/steps/step.dart';
+export 'src/steps/step_not_defined_exception.dart';
+export 'src/survey_kit.dart';
 export 'src/task/identifier/task_identifier.dart';
 export 'src/task/navigable_task.dart';
 export 'src/task/ordered_task.dart';
 export 'src/task/task.dart';
-
+export 'src/task/task_not_defined_exception.dart';
 export 'src/views/boolean_answer_view.dart';
 export 'src/views/completion_view.dart';
+export 'src/views/date_answer_view.dart';
+export 'src/views/decoration/input_decoration.dart';
 export 'src/views/double_answer_view.dart';
+export 'src/views/image_answer_view.dart';
 export 'src/views/instruction_view.dart';
 export 'src/views/integer_answer_view.dart';
 export 'src/views/multiple_choice_answer_view.dart';
 export 'src/views/scale_answer_view.dart';
 export 'src/views/single_choice_answer_view.dart';
 export 'src/views/text_answer_view.dart';
-export 'src/views/date_answer_view.dart';
 export 'src/views/time_answer_view.dart';
-
-export 'src/views/decoration/input_decoration.dart';
 export 'src/views/widget/selection_list_tile.dart';
 export 'src/views/widget/step_view.dart';
-
-export 'src/controller/survey_controller.dart';
-
-export 'src/task/task_not_defined_exception.dart';
-export 'src/steps/step_not_defined_exception.dart';
-export 'src/navigator/rules/rule_not_defined_exception.dart';
-export 'src/steps/predefined_steps/answer_format_not_defined_exception.dart';
-
-export 'src/widget/survey_progress_configuration.dart';
-export 'src/widget/survey_progress.dart';
 export 'src/views/widget/survey_app_bar.dart';
-
-export 'src/configuration/app_bar_configuration.dart';
+export 'src/widget/survey_progress.dart';
+export 'src/widget/survey_progress_configuration.dart';
