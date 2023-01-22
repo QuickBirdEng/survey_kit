82d9fb2bfd79b43a67e8a7cd0e6fb87331e9c8ba
Marvin März <marvin.maerz@gmail.com>
2023-01-22 19:40:22 +0100

    feat/remove center

--- lib/src/view/step_view.dart
+++ lib/src/view/step_view.dart
@@ -31,59 +31,57 @@ class _StepViewState extends State<StepView> {
     final _surveyController =
         widget.controller ?? SurveyConfiguration.of(context).surveyController;
 
-    return Center(
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.background,
-        ),
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return Container(
-              constraints: const BoxConstraints(maxWidth: 600),
-              child: SingleChildScrollView(
-                padding: SurveyConfiguration.of(context).padding,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    widget.child,
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 32.0),
-                      child: OutlinedButton(
-                        onPressed: widget.isValid || !widget.step.isMandatory
-                            ? () => _surveyController.nextStep(
-                                  context,
-                                  widget.resultFunction ??
-                                      () {
-                                        return StepResult<void>(
-                                          id: widget.step.id,
-                                          result: null,
-                                          endTime: DateTime.now(),
-                                          startTime: startTime,
-                                        );
-                                      },
-                                )
-                            : null,
-                        child: Text(
-                          widget.step.buttonText ??
-                              context
-                                  .read<Map<String, String>?>()?['next']
-                                  ?.toUpperCase() ??
-                              'Next',
-                          style: TextStyle(
-                            color: widget.isValid
-                                ? Theme.of(context).primaryColor
-                                : Colors.grey,
-                          ),
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.background,
+      ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Container(
+            constraints: const BoxConstraints(maxWidth: 600),
+            child: SingleChildScrollView(
+              padding: SurveyConfiguration.of(context).padding,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  widget.child,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 32.0),
+                    child: OutlinedButton(
+                      onPressed: widget.isValid || !widget.step.isMandatory
+                          ? () => _surveyController.nextStep(
+                                context,
+                                widget.resultFunction ??
+                                    () {
+                                      return StepResult<void>(
+                                        id: widget.step.id,
+                                        result: null,
+                                        endTime: DateTime.now(),
+                                        startTime: startTime,
+                                      );
+                                    },
+                              )
+                          : null,
+                      child: Text(
+                        widget.step.buttonText ??
+                            context
+                                .read<Map<String, String>?>()?['next']
+                                ?.toUpperCase() ??
+                            'Next',
+                        style: TextStyle(
+                          color: widget.isValid
+                              ? Theme.of(context).primaryColor
+                              : Colors.grey,
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
