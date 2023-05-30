800c3c1e02e35dc6066b60cf5c7352fc048c89dc
Marvin März <marvin.maerz@gmail.com>
2023-05-30 11:04:35 +0200

    fix: loading previous saved result

--- lib/src/view/widget/answer/answer_mixin.dart
+++ lib/src/view/widget/answer/answer_mixin.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:survey_kit/src/view/widget/question_answer.dart';
 
 mixin AnswerMixin<T extends StatefulWidget, R> on State<T> {
-  @override
-  void initState() {
-    super.initState();
-    SchedulerBinding.instance.addPostFrameCallback((_) {
-      onChange(QuestionAnswer.of(context).stepResult?.result as R?);
-    });
-  }
-
   void onChange(R? result) {
     onValidationChanged = isValid(result);
     onStepResultChanged = result;
