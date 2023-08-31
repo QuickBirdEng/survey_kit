bd5807f693a7e80a1c1eb83e981fe97f4331e800
Gabriel Soares <gabriel.soares@simrede.com.br>
2023-08-31 16:40:13 -0300

    ajustment in the TextField Focus

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,7 +27,7 @@ class _MyAppState extends State<MyApp> {
           child: Align(
             alignment: Alignment.center,
             child: FutureBuilder<Task>(
-              future: getJsonTask(),
+              future: getSampleTask(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.done &&
                     snapshot.hasData &&
@@ -171,92 +171,9 @@ class _MyAppState extends State<MyApp> {
           text: 'Get ready for a bunch of super random questions!',
           buttonText: 'Let\'s go!',
         ),
-        QuestionStep(
-          title: 'How old are you?',
-          answerFormat: IntegerAnswerFormat(
-            defaultValue: 25,
-            hint: 'Please enter your age',
-          ),
-          isOptional: true,
-        ),
-        QuestionStep(
-          title: 'Medication?',
-          text: 'Are you using any medication',
-          answerFormat: BooleanAnswerFormat(
-            positiveAnswer: 'Yes',
-            negativeAnswer: 'No',
-            result: BooleanResult.POSITIVE,
-          ),
-        ),
-        QuestionStep(
-          title: 'Tell us about you',
-          text:
-              'Tell us about yourself and why you want to improve your health.',
-          answerFormat: TextAnswerFormat(
-            maxLines: 5,
-            validationRegEx: "^(?!\s*\$).+",
-          ),
-        ),
-        QuestionStep(
-          title: 'Select your body type',
-          answerFormat: ScaleAnswerFormat(
-            step: 1,
-            minimumValue: 1,
-            maximumValue: 5,
-            defaultValue: 3,
-            minimumValueDescription: '1',
-            maximumValueDescription: '5',
-          ),
-        ),
-        QuestionStep(
-          title: 'Known allergies',
-          text: 'Do you have any allergies that we should be aware of?',
-          isOptional: false,
-          answerFormat: MultipleChoiceAnswerFormat(
-            textChoices: [
-              TextChoice(text: 'Penicillin', value: 'Penicillin'),
-              TextChoice(text: 'Latex', value: 'Latex'),
-              TextChoice(text: 'Pet', value: 'Pet'),
-              TextChoice(text: 'Pollen', value: 'Pollen'),
-            ],
-          ),
-        ),
-        QuestionStep(
-          title: 'Done?',
-          text: 'We are done, do you mind to tell us more about yourself?',
-          isOptional: true,
-          answerFormat: SingleChoiceAnswerFormat(
-            textChoices: [
-              TextChoice(text: 'Yes', value: 'Yes'),
-              TextChoice(text: 'No', value: 'No'),
-            ],
-            defaultSelection: TextChoice(text: 'No', value: 'No'),
-          ),
-        ),
-        QuestionStep(
-          title: 'When did you wake up?',
-          answerFormat: TimeAnswerFormat(
-            defaultValue: TimeOfDay(
-              hour: 12,
-              minute: 0,
-            ),
-          ),
-        ),
-        QuestionStep(
-          title: 'When was your last holiday?',
-          answerFormat: DateAnswerFormat(
-            minDate: DateTime.utc(1970),
-            defaultDate: DateTime.now(),
-            maxDate: DateTime.now(),
-          ),
-        ),
-        QuestionStep(
-          title: 'Upload a image of you',
-          answerFormat: ImageAnswerFormat(
-            buttonText: 'Upload your photo',
-            useGallery: true,
-          ),
-        ),
+        QuestionStep(answerFormat: IntegerAnswerFormat(hint: 'teste 1')),
+        QuestionStep(answerFormat: DoubleAnswerFormat(hint: 'teste 2')),
+        QuestionStep(answerFormat: TextAnswerFormat(hint: 'teste 3')),
         CompletionStep(
           stepIdentifier: StepIdentifier(id: '321'),
           text: 'Thanks for taking the survey, we will contact you soon!',
@@ -266,14 +183,14 @@ class _MyAppState extends State<MyApp> {
       ],
     );
     task.addNavigationRule(
-      forTriggerStepIdentifier: task.steps[6].stepIdentifier,
+      forTriggerStepIdentifier: task.steps[2].stepIdentifier,
       navigationRule: ConditionalNavigationRule(
         resultToStepIdentifierMapper: (input) {
           switch (input) {
             case "Yes":
               return task.steps[0].stepIdentifier;
             case "No":
-              return task.steps[7].stepIdentifier;
+              return task.steps[3].stepIdentifier;
             default:
               return null;
           }
