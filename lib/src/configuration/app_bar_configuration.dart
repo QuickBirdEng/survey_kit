d0a2c79328d0cd522e3bf04f88f5702656c4745a
Adar <saiful@apexive.com>
2023-12-14 16:28:08 +0600

    Make cancel button configurable in the AppBar (#139)
    * added missing MultipleChoiceQuestionResult toJson
    
    * Updated PlatformAppbar with default AppBar widget
    
    * Updated IconButton -> BackButton
    
    * Added other field in Multiple choice answers
    
    * added Multiple choice autocomplete
    
    * Customized the builder
    
    * added comment
    
    * DRY refactor
    
    * Updated other field label behavior
    
    * Added clear button and added tick mark check on dropdown
    
    * Updated example
    
    * Removed comment
    
    * json file updated
    
    * added default value for text question
    
    * Boolean Answer defaultValue
    
    * put back actual example_json.json
    
    * Fixed consistent focus in non textfield views
    
    There was a problem that keyboard was not hiding after navigating to
    next question when the view is not text/double answer view
    
    * added showCancelButton flag
    
    * update build_runner dependency
    
    * update flutter changes for android directory
    
    * update flutter changes for ios directory
    
    * update example pubspec.lock
    
    * add missing JsonSerializable annotation for ImageQuestionResult
    
    * re-run build_runner latest verson
    
    * remove duplicate imports

--- lib/src/configuration/app_bar_configuration.dart
+++ lib/src/configuration/app_bar_configuration.dart
@@ -5,10 +5,12 @@ class AppBarConfiguration {
   final bool? canBack;
   final bool? showProgress;
   final Widget? trailing;
+  final bool? showCancelButton;
 
   const AppBarConfiguration({
     required this.canBack,
     required this.showProgress,
+    this.showCancelButton = true,
     this.leading,
     this.trailing,
   });
