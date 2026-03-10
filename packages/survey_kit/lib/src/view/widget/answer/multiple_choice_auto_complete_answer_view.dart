3fc661b78a91718035309b5fb0e0b5aa18a6a68b
Marvin März <marvin.maerz@quickbird.io>
2026-03-10 17:12:31 +0100

    refactor: Add AnswerMixin import to MultipleChoiceAutoCompleteAnswerView.

--- packages/survey_kit/lib/src/view/widget/answer/multiple_choice_auto_complete_answer_view.dart
+++ packages/survey_kit/lib/src/view/widget/answer/multiple_choice_auto_complete_answer_view.dart
@@ -8,6 +8,7 @@ import 'package:survey_kit/src/model/step.dart';
 import 'package:survey_kit/src/util/measure_date_state_mixin.dart';
 import 'package:survey_kit/src/view/widget/answer/answer_question_text.dart';
 import 'package:survey_kit/src/view/widget/answer/selection_list_tile.dart';
+import 'package:survey_kit/src/view/widget/answer/answer_mixin.dart';
 
 class MultipleChoiceAutoCompleteAnswerView extends StatefulWidget {
   final Step questionStep;
